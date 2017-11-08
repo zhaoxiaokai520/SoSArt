26a8916fbc0ffb81f086ecf95f6d414fd683f544
zhaoxiaokai <836963862@qq.com>
2017-11-08 17:24:42 +0800

    add network module

---
+++ deplibs/NetLibDll/NetLibDll/xlog/include/mars/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION ""
+#define MARS_PATH ""
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2017-11-08 10:04:32"
+#define MARS_TAG "win_out_[2017-11-08_10.04]@@Administrator"
+
+#endif
